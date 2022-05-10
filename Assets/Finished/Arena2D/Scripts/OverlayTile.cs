23248023caeccb40f38fa44c2da6c3e5793bbda1
kopp20 <56162079+kopp20@users.noreply.github.com>
2022-05-10 16:43:14 +0200

    a

---
+++ Assets/Finished/Arena2D/Scripts/OverlayTile.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverlayTile : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
+        }
+    }
+}
+
